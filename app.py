ca5ac0e9bb577adbda91f32e506bf8631b8f7ec1
SoloDiyo <selotdhiyo@gmail.com>
2024-09-16 14:04:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__, template_folder='template')
+lis = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        textbox_value = random.randint(1,90)
+        lis.append(int(textbox_value))
+        lis.sort()
+            # Process the textbox value here (e.g., print it, store it in a database)
+        return render_template('main2.html', value=lis)
+    else:
+        lis.clear()
+        return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
